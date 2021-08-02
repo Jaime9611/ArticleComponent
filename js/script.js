333f87c5a5f307a50962ca6a8d8e3ebbc59efd54
Jaime9611 <jaimeescobar9611@gmail.com>
2021-08-02 15:03:32 -0500

    Javascript added to the share button

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const article_profile = document.querySelector(".article--info__profile");
+const article_info = document.querySelector(".article--info");
+const article_share = document.querySelector(".article--info__share");
+const share_btn = document.querySelector(".btn-share");
+
+function activate_share() {
+  if (article_info.classList.contains("share-active")) {
+    article_profile.classList.remove("share-active");
+    article_info.classList.remove("share-active");
+    article_share.classList.remove("share-active");
+  } else {
+    article_profile.classList.add("share-active");
+    article_info.classList.add("share-active");
+    article_share.classList.add("share-active");
+  }
+}
+
+share_btn.addEventListener("click", () => {
+  activate_share();
+});
